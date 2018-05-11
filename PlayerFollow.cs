13d9aeca2e07d5c5a5588d927eff635ed3007db2
Grigor Zotov <grigorzotov@gmail.com>
2018-05-11 21:12:40 +0300

    Add files via upload

---
+++ PlayerFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 desiredPostion = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPostion, smoothSpeed );
+
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
+
+	}
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 300f;
+    public float sidewaysForce = 100f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Move Foreward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //Controlls
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("left")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManagerSrc>().GameOver();
+        }
+           
+     }
+	
+}
